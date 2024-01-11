e49cd359827a5a4027af88ec42077ae83be79179
peterxcli <peterxcli@gmail.com>
2024-01-11 15:09:40 +0800

    fix: Remove Id field from PsychoTest struct because `gorm.Model` already has the ID field

--- pkg/model/PsychoTest.go
+++ pkg/model/PsychoTest.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 type PsychoTest struct {
 	gorm.Model
-	Id    int    `gorm:"type:int;primary_key"`
 	Type  string `gorm:"type:varchar(255);unique"`
 	Count int    `gorm:"type:int"`
 }
